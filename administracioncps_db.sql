acf37b49c468f34dcc69b97049cb89ea88858aa3
Johan40Dev <114191349+Johan40Dev@users.noreply.github.com>
2022-10-22 14:14:17 -0500

    Add files via upload

---
+++ administracioncps_db.sql
@@ -0,0 +1,114 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 22-10-2022 a las 21:13:54
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `administracioncps_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `administrador`
+--
+
+CREATE TABLE `administrador` (
+  `CODIGO` varchar(20) NOT NULL,
+  `NOMBRE` varchar(30) NOT NULL,
+  `APELLIDOS` varchar(50) NOT NULL,
+  `CORREO` varchar(256) NOT NULL,
+  `TELEFONO1` varchar(10) NOT NULL,
+  `TELEFONO2` varchar(10) NOT NULL,
+  `CLAVE` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `inmueble`
+--
+
+CREATE TABLE `inmueble` (
+  `CODIGO` varchar(20) NOT NULL,
+  `DESCRIPCION` varchar(200) NOT NULL,
+  `ID_RESIDENTE` varchar(20) NOT NULL,
+  `TARIFA_ADMIN` tinyint(1) NOT NULL,
+  `FECHA_PAGO` date NOT NULL,
+  `VALOR_PAGO` int(11) NOT NULL,
+  `MES_ACTUAL` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `parametros`
+--
+
+CREATE TABLE `parametros` (
+  `TARIFA_ADMINISTRACION` int(11) NOT NULL,
+  `TARIFA_SERVICIOS` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `residente`
+--
+
+CREATE TABLE `residente` (
+  `CODIGO` varchar(20) NOT NULL,
+  `NOMBRE` varchar(30) NOT NULL,
+  `APELLIDOS` varchar(10) NOT NULL,
+  `CORREO` varchar(256) NOT NULL,
+  `TELEFONO1` varchar(10) NOT NULL,
+  `TELEFONO2` varchar(10) NOT NULL,
+  `CLAVE` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `administrador`
+--
+ALTER TABLE `administrador`
+  ADD PRIMARY KEY (`CODIGO`);
+
+--
+-- Indices de la tabla `inmueble`
+--
+ALTER TABLE `inmueble`
+  ADD PRIMARY KEY (`CODIGO`);
+
+--
+-- Indices de la tabla `parametros`
+--
+ALTER TABLE `parametros`
+  ADD PRIMARY KEY (`TARIFA_ADMINISTRACION`);
+
+--
+-- Indices de la tabla `residente`
+--
+ALTER TABLE `residente`
+  ADD PRIMARY KEY (`CODIGO`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
